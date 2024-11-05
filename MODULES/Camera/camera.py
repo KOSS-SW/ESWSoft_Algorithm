785d35a142e5e3472dc4fff0cced2e38756f81eb
inwoo <inwooha00@naver.com>
2024-11-05 22:40:49 +0900

    fix: fatel bug

--- MODULES/Camera/camera.py
+++ MODULES/Camera/camera.py
@@ -152,7 +152,7 @@ class Cam:
     def ball_hitable(self, bc):
         dis = [(bc[0] - Cam.HIT_SPOT[0]), (bc[1] - Cam.HIT_SPOT[1])]
         self.logger.debug(dis)
-        z = list(map(lambda x: x < (Cam.ERROR//2), dis))
+        z = list(map(lambda x: abs(x) < (Cam.ERROR//2), dis))
         self.logger.debug(z)
         return z[0], z[1], dis[0], dis[1]
         
