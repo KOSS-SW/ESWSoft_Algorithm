02ac76f27d62458dcb23006d646d9e12240f31d0
StandGive <SeoCoding123@gmail.com>
2024-11-06 22:03:08 +0900

    FIX(main.py): 에러 해결

--- main.py
+++ main.py
@@ -343,10 +343,10 @@ while True:
                 is_ball, bc = cam.detect_ball()  # 공 검출 시도
 
                 if is_ball:  # 공이 검출되면
-                if is_ball and not cam.ball_left(bc):  # 공이 검출되면
-                    checkIn = True
-                    bot.task2following()  # 한번 공을 친 후, following 테스크로 이동
-                    break  # 루프 종료
+                    if is_ball and not cam.ball_left(bc):  # 공이 검출되면
+                        checkIn = True
+                        bot.task2following()  # 한번 공을 친 후, following 테스크로 이동
+                        break  # 루프 종료
 
                 bot.body_left_10()  # 공이 검출되지 않으면 왼쪽으로 회전
 
