4e74c60e4af7e39153dd39ff47b1834c840d3575
inwoo <inwooha00@naver.com>
2024-11-13 22:16:33 +0900

    FIX: add turn 4,6 in 90 degree

--- MODULES/Motion/robot.py
+++ MODULES/Motion/robot.py
@@ -204,6 +204,7 @@ class Bot:
         self.__TX_data(4)
         self.__TX_data(4)
         self.__TX_data(4)
+        self.__TX_data(4)
 
     def body_right_90(self):
         '''로봇 오른쪽으로 90도 회전 '''
@@ -211,6 +212,7 @@ class Bot:
         self.__TX_data(9)
         self.__TX_data(9)
 
+        self.__TX_data(6)
         self.__TX_data(6)
         self.__TX_data(6)
         self.__TX_data(6)
