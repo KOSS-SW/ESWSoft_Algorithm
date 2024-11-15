3ff1abbd2f82f740e368f8cdd284e1725d5943c9
inwoo <inwooha00@naver.com>
2024-11-15 16:56:32 +0900

    .

--- main.py
+++ main.py
@@ -235,12 +235,13 @@ while True:
             while True:
                 cam.read()
                 is_flag, fc = cam.detect_flag()
-                if is_flag and cam.flag_is_center(fc, -100):
+                if is_flag and cam.flag_is_center(fc, 100):
                     break
                 if cam.flag_left(fc):
                     bot.body_left_10()
                 else:
                     bot.body_right_5()
+                time.sleep(0.2)
             logger.info("set 90 done")
             set90 = False
             bot.head_down_35()
