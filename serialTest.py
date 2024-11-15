2f0932edde75f22c1e17fc7c2eaf95e55c6ddba4
inwoo <inwooha00@naver.com>
2024-11-15 14:17:07 +0900

    fix error

--- serialTest.py
+++ serialTest.py
@@ -6,4 +6,4 @@ serial_ = serial.Serial('/dev/ttyS0', 4800, timeout=0.01)
 serial_.flush() # serial cls
 
 
-serial_.write(serial.to_bytes([sys.argv[1]]))
+serial_.write(serial.to_bytes([int(sys.argv[1])]))
