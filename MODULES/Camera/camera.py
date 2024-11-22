8ea37f406f4dbaffc7ba6d3671d0e7f1c958b8cb
inwoo <inwooha00@naver.com>
2024-11-22 15:30:15 +0900

    .

--- MODULES/Camera/camera.py
+++ MODULES/Camera/camera.py
@@ -17,7 +17,7 @@ class Cam:
     ERROR = 10
     DEBUG = False
     MIN_AREA = [5,60]
-    HIT_SPOT = (426,206.1411764706)
+    HIT_SPOT = (426,206)
 
     hsv_Lower_boll = (0, 108, 163)
     hsv_Upper_boll = (255, 176, 255) 
