c6c73d4dcd7e0ff4a51a94ce22a8516c7d384759
inwoo <inwooha00@naver.com>
2024-11-26 22:24:23 +0900

    change par3 38 2 2

--- MODULES/Motion/robot.py
+++ MODULES/Motion/robot.py
@@ -310,7 +310,7 @@ class Bot:
                 self.__TX_data(5)
         else:
             if self.hitting in [0,1]:
-                self.__TX_data(38)
+                self.__TX_data(2)
         self.hitting += 1
             
     def ready_x(self, x):
