c072ab13ef40c67638e1d888a7e60afdcac92809
inwoo <inwooha00@naver.com>
2024-11-26 20:05:43 +0900

    delete nodebug

--- main.py
+++ main.py
@@ -40,6 +40,7 @@ par4 = False
 
 logger.info("main intializing")
 bot = Bot()
+cam = Cam(True)
 logger.info("bot True")
 
 if __name__ == "__main__":
@@ -49,10 +50,6 @@ if __name__ == "__main__":
         print("par4 is True")
     if 'down' in sys.argv:
         bot.head_down_65()
-    if 'nodebug' in sys.argv:
-        cam = Cam(False)
-    else:
-        cam = Cam(True)
 
 
 while True:
